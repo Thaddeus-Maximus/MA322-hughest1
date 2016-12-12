c74b807c40693428a51d10751c0b8b39ca824e42
Thaddeus Hughes <Thaddeus Hughes>
2016-12-11 21:27:12 -0500

    Backsolve works, just no errors

--- Hughes_Backsolve.m
+++ Hughes_Backsolve.m
@@ -1,12 +1,13 @@
 function [x,status]=Hughes_Backsolve(L,U,p,b)
-  y = zeros(size(L, 1), 1)
-  for i=1:size(L,1)
-      if(i>1)
-        leftover =L(i,1:i-1)*y(i-1);
-      else
-        leftover=0;
-      end
-      leftover
-      y(i)=b(i)-leftover;
-  end
-  y
+    status=0;
+    %2 inconsistent
+    %1 consistent infinite sols
+    y=zeros(size(U,2),1);
+    for i = 1:size(L,1)
+        y(i)=(b(p(i)) - L(i,1:i-1)*y(1:i-1))/L(i,i);
+    end
+    
+    x=zeros(size(U,2),1);
+    for i = size(U,2):-1:1
+        x(i)=(y(i) - U(i,i+1:end)*x(i+1:end))/U(i,i);
+    end
